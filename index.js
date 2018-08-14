79d740a53661a0736a741085bea650a589bb9dc2
David C <davidthedev@users.noreply.github.com>
2018-08-14 22:22:34 +0100

    Add multi-client support

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
 const server = require('net').createServer();
+let counter = 0;
+let sockets = {};
 
 server.on('connection', socket => {
+  socket.id = counter++;
+  sockets[socket.id] = socket;
+
   console.log('Client connected');
   socket.write('Welcome new client!\n');
 
   socket.on('data', data => {
     // log data as Buffer
-    console.log('data is:', data);
+    console.log('socket id: ' + socket.id + ', data is: ', data);
     // send data back to client with the default utf8 encoding
-    socket.write(`data is: ${data}`);
+    Object.entries(sockets).forEach(([key, cs]) => {
+      cs.write(`${socket.id} ${data}`);
+    });
   });
 
   socket.on('end', () => {
+    delete sockets[socket.id];
     console.log('Client disconnected');
   });
 });
