3acfccd5e5dec1a709633ab5c0f92c578f1b646b
David C <davidthedev@users.noreply.github.com>
2018-08-14 22:48:35 +0100

    Add the ability to enter name

--- index.js
+++ index.js
@@ -2,19 +2,34 @@ const server = require('net').createServer();
 let counter = 0;
 let sockets = {};
 
+function getTimestamp() {
+  const time = new Date();
+  return `${time.getHours()}:${time.getMinutes()}:${time.getSeconds()}`;
+}
+
 server.on('connection', socket => {
   socket.id = counter++;
-  sockets[socket.id] = socket;
 
   console.log('Client connected');
-  socket.write('Welcome new client!\n');
+  socket.write('Please type your name: ');
 
   socket.on('data', data => {
+
+    if (!sockets[socket.id]) {
+      socket.name = data.toString().trim();
+      socket.write(`Welcome ${socket.name}!\n`);
+      sockets[socket.id] = socket;
+      return;
+    }
+
     // log data as Buffer
-    console.log('socket id: ' + socket.id + ', data is: ', data);
+    // console.log('socket id: ' + socket.id + ', data is: ', data);
+
     // send data back to client with the default utf8 encoding
     Object.entries(sockets).forEach(([key, cs]) => {
-      cs.write(`${socket.id} ${data}`);
+      // do not send message to the client that posted it
+      if (socket.id == key) return;
+      cs.write(`${getTimestamp()} | ${socket.name} says: ${data}`);
     });
   });
 
